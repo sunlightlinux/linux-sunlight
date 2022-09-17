42639428bef8338990c5c56168c96c345a910bf1
Ionut Nechita <ionut_n2001@yahoo.com>
2022-09-17 18:07:27 +0300

    SUNLIGHT: SCHED: BORE - CPU scheduler v6.0
    Description:
     - New: SCHED: BORE - CPU scheduler v6.0
       BORE(Burst-Oriented Response Enhancer)
       1.4.31.2
    
    Bug: N/A
    Change-Id: If395e9f21ebd58325ef0bb55c8baf385ce924bd1
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -317,6 +317,12 @@ static __init int sched_init_debug(void)
 
 	debugfs_create_u32("latency_warn_ms", 0644, debugfs_sched, &sysctl_resched_latency_warn_ms);
 	debugfs_create_u32("latency_warn_once", 0644, debugfs_sched, &sysctl_resched_latency_warn_once);
+#ifdef CONFIG_SCHED_BORE
+	debugfs_create_u16("burst_penalty_scale", 0644, debugfs_sched, &sched_burst_penalty_scale);
+	debugfs_create_u8("burst_granularity", 0644, debugfs_sched, &sched_burst_granularity);
+	debugfs_create_u8("burst_reduction", 0644, debugfs_sched, &sched_burst_reduction);
+	debugfs_create_bool("burst_preempt", 0644, debugfs_sched, &sched_burst_preempt);
+#endif // CONFIG_SCHED_BORE
 
 #ifdef CONFIG_SMP
 	debugfs_create_file("tunable_scaling", 0644, debugfs_sched, NULL, &sched_scaling_fops);
