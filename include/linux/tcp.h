df97f7ad0a3b5126f076bed465555730468b78dc
Ionut Nechita <ionut_n2001@yahoo.com>
2024-02-04 15:05:37 +0200

    SUNLIGHT: net-tcp: add fast_ack_mode=1: skip rwin check in  tcp_fast_ack_mode__tcp_ack_snd_check()
    Add logic for an experimental TCP connection behavior, enabled with
    tp->fast_ack_mode = 1, which disables checking the receive window
    before sending an ack in __tcp_ack_snd_check(). If this behavior is
    enabled, the data receiver sends an ACK if the amount of data is >
    RCV.MSS.
    
    Change-Id: Iaa0a0fd7108221f883137a79d5bfa724f1b096d4
    Signed-off-by: Alexandre Frade <kernel@xanmod.org>
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -369,7 +369,9 @@ struct tcp_sock {
 	u8	compressed_ack;
 	u8	dup_ack_counter:2,
 		tlp_retrans:1,	/* TLP is a retransmission */
-		unused:5;
+		tcp_usec_ts:1, /* TSval values in usec */
+		fast_ack_mode:2, /* which fast ack mode ? */
+		unused:2;
 	u8	thin_lto    : 1,/* Use linear timeouts for thin streams */
 		recvmsg_inq : 1,/* Indicate # of bytes in queue upon recvmsg */
 		fastopen_connect:1, /* FASTOPEN_CONNECT sockopt */
