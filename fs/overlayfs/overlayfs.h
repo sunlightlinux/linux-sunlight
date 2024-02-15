cf97ea9641ea02a34275184bcc465f040f82feee
Ionut Nechita <ionut_n2001@yahoo.com>
2024-02-15 09:53:57 +0200

    SUNLIGHT: Use default behaviour from upstream for overlayfs
    Description:
     -  Use default behaviour from upstream for overlayfs
    
    Bug: N/A
    Change-Id: Iafbd64892c564492cef51a4268106095856c3e29
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- fs/overlayfs/overlayfs.h
+++ fs/overlayfs/overlayfs.h
@@ -426,7 +426,6 @@ int ovl_want_write(struct dentry *dentry);
 void ovl_drop_write(struct dentry *dentry);
 struct dentry *ovl_workdir(struct dentry *dentry);
 const struct cred *ovl_override_creds(struct super_block *sb);
-void ovl_revert_creds(struct super_block *sb, const struct cred *oldcred);
 
 static inline const struct cred *ovl_creds(struct super_block *sb)
 {
