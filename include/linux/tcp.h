854bab5a8f39e93eced517b7a6a5b00465729a7f
Ionut Nechita <ionut_n2001@yahoo.com>
2024-03-09 15:01:38 +0200

    SUNLIGHT: Minor change for bbr in tcp_sock
    Description:
     - Minor change for bbr in tcp_sock
    
    Change-Id: If462b8f888e4f424166cbb0c33ab6ce86e730125
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -369,10 +369,9 @@ struct tcp_sock {
 	u8	compressed_ack;
 	u8	dup_ack_counter:2,
 		tlp_retrans:1,	/* TLP is a retransmission */
-		tcp_usec_ts:1, /* TSval values in usec */
 		fast_ack_mode:2, /* which fast ack mode ? */
 		tlp_orig_data_app_limited:1, /* app-limited before TLP rtx? */
-		unused:1;
+		unused:2;
 	u8	thin_lto    : 1,/* Use linear timeouts for thin streams */
 		recvmsg_inq : 1,/* Indicate # of bytes in queue upon recvmsg */
 		fastopen_connect:1, /* FASTOPEN_CONNECT sockopt */
