6f9766caafe248ec4275cdf02c202a1fece1286a
Jeff Mahoney <jeffm@suse.com>
2024-05-12 16:26:01 +0300

    SUNLIGHT: vfs: add super_operations->get_inode_dev
    There are many places where a dev_t:ino_t pair are passed to userspace
    to uniquely describe an inode.  Some file systems, like btrfs, have
    multiple inode namespace internally and use a separate dev_t to make the
    distinction between them.
    
    This patch adds a super_operations->get_inode_dev operation to allow
    the file system to export those dev_ts to callers within the kernel
    for further export to userspace.
    
    Without this patch, things like audit and some perf and trace events
    will not distinguish between subvolumes within a single btrfs filesystem.
    
    Change-Id: Id48ff58b3d178712db591ad5e7701fc4a62acd59
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- fs/nfsd/vfs.c
+++ fs/nfsd/vfs.c
@@ -1131,7 +1131,7 @@ static int wait_for_concurrent_writes(struct file *file)
 	int err = 0;
 
 	if (atomic_read(&inode->i_writecount) > 1
-	    || (last_ino == inode->i_ino && last_dev == inode->i_sb->s_dev)) {
+	    || (last_ino == inode->i_ino && last_dev == inode_get_dev(inode))) {
 		dprintk("nfsd: write defer %d\n", task_pid_nr(current));
 		msleep(10);
 		dprintk("nfsd: write resume %d\n", task_pid_nr(current));
@@ -1142,7 +1142,7 @@ static int wait_for_concurrent_writes(struct file *file)
 		err = vfs_fsync(file, 0);
 	}
 	last_ino = inode->i_ino;
-	last_dev = inode->i_sb->s_dev;
+	last_dev = inode_get_dev(inode);
 	return err;
 }
 
