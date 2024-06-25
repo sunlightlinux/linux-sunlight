ca8b27c51f0962f8fb59e5acb23e0af791fb5c04
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2024-06-25 10:56:32 +0200

    perf: Update the perf series
    This is an all-in-one patch integrating the following changes:
    
    - Merging Frederick's "Fix leaked sigtrap events" series as of v4 which
      is a dependency.
    
    - Update the "perf test sigtrap" fixup to v4 as posted.
    
    Link: https://lore.kernel.org/20240621091601.18227-1-frederic@kernel.org
    Link: https://lore.kernel.org/20240624152732.1231678-1-bigeasy@linutronix.de
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -786,6 +786,7 @@ struct perf_event {
 	struct irq_work			pending_disable_irq;
 	struct callback_head		pending_task;
 	unsigned int			pending_work;
+	struct rcuwait			pending_work_wait;
 
 	atomic_t			event_limit;
 
@@ -969,12 +970,6 @@ struct perf_event_context {
 	 */
 	local_t				nr_pending;
 };
-
-/*
- * Number of contexts where an event can trigger:
- *	task, softirq, hardirq, nmi.
- */
-#define PERF_NR_CONTEXTS	4
 
 struct perf_cpu_pmu_context {
 	struct perf_event_pmu_context	epc;
