63f88c546cde7302f5b7cfe260a0fcbbdb456afd
Ionut Nechita <ionut_n2001@yahoo.com>
2024-10-13 20:30:53 +0300

    SUNLIGHT: Minor change in bluetooth for btusb.c
    Description:
     - Minor change in bluetooth for btusb.c
    
                    if (data->reset_gpio)
                            gpiod_put(data->reset_gpio);
    
                    bt_dev_warn(data->hdev, "Failed to claim iso interface: %d", err);
    
    Change-Id: Iccfa4cfdd6017e9de454cdbf58f26a0699a5bf86
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -2829,7 +2829,11 @@ static void btusb_mtk_claim_iso_intf(struct btusb_data *data)
 	device_unlock(&btmtk_data->isopkt_intf->dev);
 	if (err < 0) {
 		btmtk_data->isopkt_intf = NULL;
-		bt_dev_err(data->hdev, "Failed to claim iso interface: %d", err);
+
+		if (data->reset_gpio)
+			gpiod_put(data->reset_gpio);
+
+		bt_dev_warn(data->hdev, "Failed to claim iso interface: %d", err);
 		return;
 	}
 
