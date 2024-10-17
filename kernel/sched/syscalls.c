47041d5379feeb1dff46b0f27f76c5a47d59c8ef
Colin Ian King <colin.i.king@intel.com>
2024-10-17 16:29:50 +0100

    SUNLIGHT: handle sched_yield gracefully when being hammered
    Description:
     - Some misguided apps hammer sched_yield() in a tight loop (they should be using futexes instead)
       which causes massive lock contention even if there is little work to do or to yield to.
       rare limit yielding since the base scheduler does a pretty good job already about just
       running the right things
    
    Vendor: ClearLinux
    Bug: N/A
    Change-Id: I04e680c8b19bb7ee5671a68903691e2ca4e34ca3
    Signed-off-by: Colin Ian King <colin.i.king@intel.com>
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- kernel/sched/syscalls.c
+++ kernel/sched/syscalls.c
@@ -1357,10 +1357,22 @@ SYSCALL_DEFINE3(sched_getaffinity, pid_t, pid, unsigned int, len,
 	return ret;
 }
 
+static DEFINE_PER_CPU(unsigned long, last_yield);
+
 static void do_sched_yield(void)
 {
 	struct rq_flags rf;
 	struct rq *rq;
+	int cpu = raw_smp_processor_id();
+
+	cond_resched();
+
+	/* rate limit yielding to something sensible */
+
+	if (!time_after(jiffies, per_cpu(last_yield, cpu)))
+		return;
+
+	per_cpu(last_yield, cpu) = jiffies;
 
 	rq = this_rq_lock_irq(&rf);
 
