eba8879689e5357d555b55909574677d1b3fca0b
Ionut Nechita <ionut_n2001@yahoo.com>
2024-12-24 05:15:50 +0900

    SUNLIGHT: SCHED: BORE - CPU scheduler v6.12
    Description:
     - SCHED: BORE - CPU scheduler v6.12
       BORE(Burst-Oriented Response Enhancer)
       5.7.14
    
    Bug: N/A
    Change-Id: Ied9db2be71f71fb850c2dc175cd7b7172759c5c4
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -102,6 +102,8 @@
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/dtask.h>
 
+#include <linux/sched/bore.h>
+
 EXPORT_TRACEPOINT_SYMBOL_GPL(ipi_send_cpu);
 EXPORT_TRACEPOINT_SYMBOL_GPL(ipi_send_cpumask);
 
@@ -9391,6 +9393,10 @@ void __init sched_init(void)
 	BUG_ON(!sched_class_above(&fair_sched_class, &ext_sched_class));
 	BUG_ON(!sched_class_above(&ext_sched_class, &idle_sched_class));
 #endif
+
+#ifdef CONFIG_SCHED_BORE
+	sched_bore_init();
+#endif // CONFIG_SCHED_BORE
 
 	wait_bit_init();
 
