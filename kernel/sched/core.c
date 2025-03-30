cff73698b90c25cfc9ef4bceefcd4c164388324e
Ionut Nechita <ionut_n2001@yahoo.com>
2025-03-30 21:01:48 +0300

    SUNLIGHT: sched/core: Further optimize sched_move_task by avoiding lock acquisition when autogroup is enabled
    Description:
     - Some sched_move_task calls are useless because task_struct->sched_task_group
       might not change (remains equal to task_group of cpu_cgroup) when the system
       has autogroup enabled. This patch optimizes the process by:
    
       1. Splitting sched_change_group() into two functions: one to get the task_group
       and another to perform the actual group change
       2. Adding a new sched_needs_group_change() function to check if a change is needed
       3. Most importantly, performing this check *before* acquiring the runqueue lock
       to completely avoid lock acquisition when no change is needed
    
       This approach provides a significant performance improvement over the original
       patch, reducing the time spent in sched_move_task by 74-96% (compared to the
       57.4% improvement in the original patch) in test scenarios involving frequent
       task creation and exit.
    
    Based on the original idea from:
    Link: https://lkml.kernel.org/r/20230321064459.39421-1-wuchi.zero@gmail.com
    Change-Id: I001b40be61b3e45d5ee34b27bf385cacab461d56
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9564,7 +9564,7 @@ void sched_release_group(struct task_group *tg)
 	spin_unlock_irqrestore(&task_group_lock, flags);
 }
 
-static void sched_change_group(struct task_struct *tsk)
+static struct task_group *sched_get_task_group(struct task_struct *tsk)
 {
 	struct task_group *tg;
 
@@ -9576,14 +9576,31 @@ static void sched_change_group(struct task_struct *tsk)
 	tg = container_of(task_css_check(tsk, cpu_cgrp_id, true),
 			  struct task_group, css);
 	tg = autogroup_task_group(tsk, tg);
-	tsk->sched_task_group = tg;
 
+	return tg;
+}
+
+static void sched_change_group(struct task_struct *tsk, struct task_group *group)
+{
+	tsk->sched_task_group = group;
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	if (tsk->sched_class->task_change_group)
 		tsk->sched_class->task_change_group(tsk);
 	else
 #endif
 		set_task_rq(tsk, task_cpu(tsk));
+}
+
+static struct task_group *sched_needs_group_change(struct task_struct *tsk)
+{
+	struct task_group *new_group;
+
+	new_group = sched_get_task_group(tsk);
+
+	if (likely(new_group == tsk->sched_task_group))
+		return NULL;
+
+	return new_group;
 }
 
 /*
@@ -9595,16 +9612,20 @@ static void sched_change_group(struct task_struct *tsk)
  */
 void sched_move_task(struct task_struct *tsk, bool for_autogroup)
 {
-	unsigned int queue_flags = DEQUEUE_SAVE | DEQUEUE_MOVE;
+	struct task_group *new_group;
+	unsigned int queue_flags = DEQUEUE_SAVE | DEQUEUE_MOVE | DEQUEUE_NOCLOCK;
 	bool resched = false;
 	bool queued = false;
 	struct rq *rq;
+
+	if (!(new_group = sched_needs_group_change(tsk)))
+		return;
 
 	CLASS(task_rq_lock, rq_guard)(tsk);
 	rq = rq_guard.rq;
 
 	scoped_guard (sched_change, tsk, queue_flags) {
-		sched_change_group(tsk);
+		sched_change_group(tsk, new_group);
 		if (!for_autogroup)
 			scx_cgroup_move_task(tsk);
 		if (scope->running)
