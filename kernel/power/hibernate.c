8d6e0daa0e2d0e6eea08e9d42ffcd858e0c4be82
Mario Limonciello <mario.limonciello@amd.com>
2025-09-23 16:10:20 -0500

    SUNLIGHT: PM: hibernate: Fix hybrid-sleep
    Description:
     - Hybrid sleep will hibernate the system followed by running through
       the suspend routine.  Since the suspend routine will call pm_restrict_gfp_mask()
       pm_restore_gfp_mask() must be called before starting this sequence.
    
    Change-Id: I43345e7190e0efd83ab2d6add256c4a5f2db4d9c
    Signed-off-by: Mario Limonciello (AMD) <superm1@kernel.org>
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -875,7 +875,8 @@ int hibernate(void)
 				power_down();
 		}
 		in_suspend = 0;
-		pm_restore_gfp_mask();
+		if (hibernation_mode != HIBERNATION_SUSPEND)
+			pm_restore_gfp_mask();
 	} else {
 		pm_pr_dbg("Hibernation image restored successfully.\n");
 	}
