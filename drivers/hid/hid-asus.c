068435417a07c75d86325f18bb1baf36c0ab3260
Ionut Nechita <ionut_n2001@yahoo.com>
2025-09-25 21:32:06 +0300

    SUNLIGHT: HID: asus: Fix input registration for ROG N-Key keyboards
    Description:
     - Some ASUS ROG N-Key keyboards have multiple USB interfaces where one
       interface may not have standard input usages mapped, causing the input
       subsystem to not claim the device (HID_CLAIMED_INPUT not set). This
       results in the driver probe failing with -ENOMEM when the strict input
       registration check fails.
    
       This particularly affects device 0B05:19B6 (ROG_NKEY_KEYBOARD2) where
       interface 1 fails to register while interfaces 0 and 2 succeed.
    
       Skip the strict input registration check for devices with
       QUIRK_ROG_NKEY_KEYBOARD, similar to how QUIRK_SKIP_INPUT_MAPPING
       is handled, allowing these devices to probe successfully even when
       some interfaces don't have mappable input usages.
    
    Change-Id: I037f4827c74bd15c9cdca327bdf8d974a88beef4
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- drivers/hid/hid-asus.c
+++ drivers/hid/hid-asus.c
@@ -1221,17 +1221,22 @@ static int asus_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	 * HID_CLAIMED_INPUT is set prevents a UAF when all input devices
 	 * were freed during registration due to no usages being mapped,
 	 * leaving drvdata->input pointing to freed memory.
+	 * Skip this check for devices with QUIRK_SKIP_INPUT_MAPPING or
+	 * ROG N-KEY devices which may have interfaces without standard input usages.
 	 */
-	if (!drvdata->input || !(hdev->claimed & HID_CLAIMED_INPUT)) {
+	if (!(drvdata->quirks & (QUIRK_SKIP_INPUT_MAPPING | QUIRK_ROG_NKEY_KEYBOARD)) &&
+	    (!drvdata->input || !(hdev->claimed & HID_CLAIMED_INPUT))) {
 		hid_warn(hdev, "Asus input not registered\n");
 		ret = -ENOMEM;
 		goto err_stop_hw;
 	}
 
-	if (drvdata->tp) {
-		drvdata->input->name = "Asus TouchPad";
-	} else {
-		drvdata->input->name = "Asus Keyboard";
+	if (drvdata->input) {
+		if (drvdata->tp) {
+			drvdata->input->name = "Asus TouchPad";
+		} else {
+			drvdata->input->name = "Asus Keyboard";
+		}
 	}
 
 	if (drvdata->tp) {
