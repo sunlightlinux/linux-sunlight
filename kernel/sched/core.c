aea0d90ffef06373c80cb1d9533a098c33db3770
Ionut Nechita <ionut_n2001@yahoo.com>
2025-10-04 14:15:31 +0300

    SUNLIGHT: sched/core: add some branch hints based on gcov analysis
    Description:
     - sched/core: add some branch hints based on gcov analysis
    
    Vendor: ClearLinux
    Bug: N/A
    Change-Id: I045e4c506d5c35a1dc49a6a54baa84edff911cac
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -640,7 +640,7 @@ void raw_spin_rq_lock_nested(struct rq *rq, int subclass)
 
 	/* Matches synchronize_rcu() in __sched_core_enable() */
 	preempt_disable();
-	if (sched_core_disabled()) {
+	if (likely(sched_core_disabled())) {
 		raw_spin_lock_nested(&rq->__lock, subclass);
 		/* preempt_count *MUST* be > 1 */
 		preempt_enable_no_resched();
@@ -857,7 +857,7 @@ void update_rq_clock(struct rq *rq)
 	scx_rq_clock_update(rq, clock);
 
 	delta = clock - rq->clock;
-	if (delta < 0)
+	if (unlikely(delta < 0))
 		return;
 	rq->clock += delta;
 
@@ -6155,7 +6155,7 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 	struct rq *rq_i;
 	bool need_sync;
 
-	if (!sched_core_enabled(rq))
+	if (likely(!sched_core_enabled(rq)))
 		return __pick_next_task(rq, prev, rf);
 
 	cpu = cpu_of(rq);
@@ -7561,7 +7561,7 @@ out_unlock:
 #if !defined(CONFIG_PREEMPTION) || defined(CONFIG_PREEMPT_DYNAMIC)
 int __sched __cond_resched(void)
 {
-	if (should_resched(0) && !irqs_disabled()) {
+	if (unlikely(should_resched(0) && !irqs_disabled())) {
 		preempt_schedule_common();
 		return 1;
 	}
