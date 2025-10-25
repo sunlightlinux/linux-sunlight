e8c80ad2812bec32fdc7a899d9b2cc4df1df7a71
Ionut Nechita <ionut_n2001@yahoo.com>
2025-10-25 17:08:26 +0300

    SUNLIGHT: SCHED: BORE - CPU scheduler v6.18
    Description:
     - SCHED: BORE - CPU scheduler v6.18
       BORE(Burst-Oriented Response Enhancer)
       6.5.8
    
    Bug: N/A
    Change-Id: Iafc519010fe80ce6be686d0f4910428a9cbd7880
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -117,6 +117,10 @@
 /* For dup_mmap(). */
 #include "../mm/internal.h"
 
+#ifdef CONFIG_SCHED_BORE
+#include <linux/sched/bore.h>
+#endif /* CONFIG_SCHED_BORE */
+
 #include <trace/events/sched.h>
 
 #define CREATE_TRACE_POINTS
@@ -2340,6 +2344,10 @@ __latent_entropy struct task_struct *copy_process(
 	 * Need tasklist lock for parent etc handling!
 	 */
 	write_lock_irq(&tasklist_lock);
+#ifdef CONFIG_SCHED_BORE
+	if (likely(p->pid))
+		task_fork_bore(p, current, clone_flags, p->start_time);
+#endif /* CONFIG_SCHED_BORE */
 
 	/* CLONE_PARENT re-uses the old parent */
 	if (clone_flags & (CLONE_PARENT|CLONE_THREAD)) {
