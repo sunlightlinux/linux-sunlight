bdaca1deb32d7e7e0b4bb3aff921d1f0857cf708
Ionut Nechita <ionut_n2001@yahoo.com>
2025-10-25 17:08:26 +0300

    SUNLIGHT: SCHED: BORE - CPU scheduler v6.18
    Description:
     - SCHED: BORE - CPU scheduler v6.18
       BORE(Burst-Oriented Response Enhancer)
       6.5.8
    
    Bug: N/A
    Change-Id: Iafc519010fe80ce6be686d0f4910428a9cbd7880
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -819,6 +819,32 @@ struct kmap_ctrl {
 #endif
 };
 
+#ifdef CONFIG_SCHED_BORE
+#define BORE_BC_TIMESTAMP_SHIFT 16
+
+struct bore_bc {
+	u64				timestamp:	48;
+	u64				penalty:	16;
+};
+
+struct bore_ctx {
+	struct bore_bc	subtree;
+	struct bore_bc	group;
+	u64				burst_time;
+	u16				prev_penalty;
+	u16				curr_penalty;
+	union {
+		u16			penalty;
+		struct {
+			u8		_;
+			u8		score;
+		};
+	};
+	bool			stop_update;
+	bool			futex_waiting;
+};
+#endif /* CONFIG_SCHED_BORE */
+
 struct task_struct {
 #ifdef CONFIG_THREAD_INFO_IN_TASK
 	/*
@@ -877,6 +903,9 @@ struct task_struct {
 #ifdef CONFIG_SCHED_CLASS_EXT
 	struct sched_ext_entity		scx;
 #endif
+#ifdef CONFIG_SCHED_BORE
+	struct bore_ctx			bore;
+#endif /* CONFIG_SCHED_BORE */
 	const struct sched_class	*sched_class;
 
 #ifdef CONFIG_SCHED_CORE
