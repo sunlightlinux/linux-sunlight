b8d08ce8fa7f981964e93a26d458968ed8aad919
Ionut Nechita <ionut_n2001@yahoo.com>
2026-01-17 14:23:17 +0200

    SUNLIGHT: sched: Update runqueue clock in sched_move_task
    Description:
     - Ensure the runqueue clock is updated before moving a task to prevent
       potential stale timestamp issues in subsequent scheduler operations.
    
    Change-Id: I277fc026af5349fa642c4e60cf17871123a648f1
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9648,6 +9648,7 @@ void sched_move_task(struct task_struct *tsk, bool for_autogroup)
 
 	CLASS(task_rq_lock, rq_guard)(tsk);
 	rq = rq_guard.rq;
+	update_rq_clock(rq);
 
 	/* Prefetch runqueue structures for better cache performance */
 	prefetch(&rq->cfs);
