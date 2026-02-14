c6a827b077984c43cad98917360849dceb5a56b2
Ionut Nechita <ionut_n2001@yahoo.com>
2026-02-14 15:32:24 +0200

    SUNLIGHT: SCHED: prefer prevcpu for wakeup v7
    Description:
     - SCHED: prefer prevcpu for wakeup v7
       Optimize task wakeup by preferring the previous CPU
    
    Bug: N/A
    Change-Id: Ief37ea5a9adc12b74dbe676eb7d414f7b3d9d26d
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2119,6 +2119,21 @@ unsigned long get_wchan(struct task_struct *p)
 	return ip;
 }
 
+static void update_rq_avg_idle(struct rq *rq)
+{
+	if (rq->idle_stamp) {
+		u64 delta = rq_clock(rq) - rq->idle_stamp;
+		u64 max = 2*rq->max_idle_balance_cost;
+
+		update_avg(&rq->avg_idle, delta);
+
+		if (rq->avg_idle > max)
+			rq->avg_idle = max;
+
+		rq->idle_stamp = 0;
+	}
+}
+
 void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 {
 	if (!(flags & ENQUEUE_NOCLOCK))
@@ -2143,6 +2158,8 @@ void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 
 	if (sched_core_enabled(rq))
 		sched_core_enqueue(rq, p);
+
+	update_rq_avg_idle(rq);
 }
 
 /*
@@ -3726,18 +3743,6 @@ ttwu_do_activate(struct rq *rq, struct task_struct *p, int wake_flags,
 		rq_unpin_lock(rq, rf);
 		p->sched_class->task_woken(rq, p);
 		rq_repin_lock(rq, rf);
-	}
-
-	if (rq->idle_stamp) {
-		u64 delta = rq_clock(rq) - rq->idle_stamp;
-		u64 max = 2*rq->max_idle_balance_cost;
-
-		update_avg(&rq->avg_idle, delta);
-
-		if (rq->avg_idle > max)
-			rq->avg_idle = max;
-
-		rq->idle_stamp = 0;
 	}
 }
 
